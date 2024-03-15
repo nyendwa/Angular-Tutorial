4fce6848775b6b190de6bd0b246a703c5377b2f4
nyendwa <nyendwa@yahoo.com>
2024-03-15 17:02:32 +0200

    update prime-icons

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -16,6 +16,9 @@ import { ConfirmationService } from 'primeng/api';
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
+  constructor(private confirmationService:ConfirmationService ) {
+    
+  }
   @Input() product!: Product;
   @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
   @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
@@ -26,6 +29,14 @@ export class ProductComponent {
 
   deleteProduct() {
     this.delete.emit(this.product);
+  }
+  confirmDelete() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete this product?',
+      accept: () => {
+        this.deleteProduct();
+      }
+    })
   }
   
    ngOnInit() {
