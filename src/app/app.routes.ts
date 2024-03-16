adaf5123bdecc438d5776c9a41723cc68b7b20c9
nyendwa <nyendwa@yahoo.com>
2024-03-16 17:39:51 +0200

    About-us feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,4 +4,11 @@ import { HomeComponent } from './home/home.component';
 export const routes: Routes = [{
     path: '',
     component:HomeComponent
-}];
+},
+    {
+        path: 'about-us',
+        loadChildren: () =>
+            import('./modules/about-us/about-us.module').then((m)=>m.AboutUsModule),
+    }
+
+];
